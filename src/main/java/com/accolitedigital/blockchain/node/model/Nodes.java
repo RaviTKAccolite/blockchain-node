28007e255d759c660b4eaa602cdd38385ba967ed
Lakshmi Susmitha Pendem <lakshmisusmitha.pendem@bt.com>
2023-03-28 12:32:04 +0530

    fix Nodes.java

--- src/main/java/com/accolitedigital/blockchain/node/model/Nodes.java
+++ src/main/java/com/accolitedigital/blockchain/node/model/Nodes.java
@@ -1,11 +1,15 @@
 package com.accolitedigital.blockchain.node.model;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Nodes {
   List<String> nodes;
 }
